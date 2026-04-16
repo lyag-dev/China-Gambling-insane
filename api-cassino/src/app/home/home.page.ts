a9e32e24c6f5e95fac1ef9751c0fa84b22ed4aed
lyag-dev <lyagushkatribola@gmail.com>
2026-04-16 15:11:44 -0300

    fizemo o bglh

--- api-cassino/src/app/home/home.page.ts
+++ api-cassino/src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { RedirectCommand } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, IonButton],
 })
 export class HomePage {
   constructor() {}
+  nm_escolhido?: number
+
+  getRandomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  comparar(){
+    let numberSorteado = this.getRandomInt(1,10)
+
+    if (numberSorteado== this.nm_escolhido){
+      return window.alert("vc ganhou, o número sorteado era: "+ numberSorteado)
+    }
+    else{
+      return window.alert("perdeu troxa, o numero era: "+ numberSorteado)
+    }
+
+  }
 }
+ 
+
+
+
